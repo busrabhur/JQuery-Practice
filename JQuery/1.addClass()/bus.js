eba78b7fa18824784de4d4e8ea44d2e49a5afc80
Büşra Buhur <bbuhur18@posta.pau.edu.tr>
2023-08-17 22:41:50 +0300

    initial

---
+++ JQuery/1.addClass()/bus.js
@@ -0,0 +1,5 @@
+$("p").last().addClass("selected"); //p etiketlerinden sonuncusuna selected classı ekle
+$("#orta").addClass("selected"); //id değeri orta olan etikete selected classını ekledi
+
+//addclass() metodu ile tek seferde bir elemente birden çok class da verebilriz
+$("p").first().addClass("selected highligthed"); 
