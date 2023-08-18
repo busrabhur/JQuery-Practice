15113574c6a112f1572a932f59c3a7cd0be46ba1
Büşra Buhur <bbuhur18@posta.pau.edu.tr>
2023-08-18 22:41:31 +0300

    attr() , hasClass()

---
+++ JQuery/2..attr()/bus.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("button").click(function(){
+        $("img").attr({width:"100",height:"100"});
+        $("button").css("color","blue");
+
+    });
+});
---
+++ JQuery/3.hasClass()/bus.js
@@ -0,0 +1,8 @@
+
+//ÖRNEK1
+    $("#result3").append($("p").hasClass("selected").toString());
+//önce appendin içindeki iç parantezi anlayalım: p etiketlerinden herhangi biri selected classına sahip mi? True
+//appendin içi TRUE oldu.//result3 idli etiketin sonuna true stringi eklenecek.
+
+
+//append() sona ekler
