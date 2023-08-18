051dcc6f6a55284595fbb379e77b3eb5cec791b5
Büşra Buhur <bbuhur18@posta.pau.edu.tr>
2023-08-18 22:50:50 +0300

    hasClass()

--- JQuery/3.hasClass()/bus.js
+++ JQuery/3.hasClass()/bus.js
@@ -1,4 +1,6 @@
 
+//hasClass()-->true ya da false döner.İlgili element verieln classa sahipse true döner
+
 //ÖRNEK1
     $("#result3").append($("p").hasClass("selected").toString());
 //önce appendin içindeki iç parantezi anlayalım: p etiketlerinden herhangi biri selected classına sahip mi? True
@@ -6,3 +8,8 @@
 
 
 //append() sona ekler
+
+//ÖRNEK2:
+$("#result1").append("<p>eklenen paragraf</p>"); //p etiketini divin içine ekledi.
+const myDiv=document.getElementById("result1").innerHTML;
+console.log(myDiv); 
